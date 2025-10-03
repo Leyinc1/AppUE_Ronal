9409e042f9ca3609c2dedbd1ce341f9f56c72fef
Manuel Ipenza <manuelipenza>
2025-10-03 17:10:42 -0500

    222

--- app/src/main/java/com/kcarbzz/myapplication/MainActivity.kt
+++ app/src/main/java/com/kcarbzz/myapplication/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.kcarbzz.myapplication.presentation.navigation.AppNavGraph
+import com.kcarbzz.myapplication.presentation.navigation.AppScreens
 import com.kcarbzz.myapplication.ui.theme.AppUETheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppUETheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                AppNavGraph(
+                    navController = navController,
+                    startDestination = AppScreens.REGISTER_ROUTE
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppUETheme {
-        Greeting("Android")
     }
 }
